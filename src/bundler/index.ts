20919664333deef0eb0ecfe1b8f439a42e18f749
MattRuetz <matt.b.ruetz@gmail.com>
2022-02-13 14:07:39 -0500

    Add code sharing betwefor styling and some UX fixes for weird behaviour.

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -1,14 +1,14 @@
-import * as esbuild from "esbuild-wasm";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 let service: esbuild.Service;
 
-const bundle =  async (rawCode: string) => {
+const bundle = async (rawCode: string) => {
     if (!service) {
         service = await esbuild.startService({
             worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
         });
     }
 
@@ -20,22 +20,21 @@ const bundle =  async (rawCode: string) => {
             plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
             define: {
                 'process.env.NODE_ENV': '"production"',
-                global: 'window'
-            }
+                global: 'window',
+            },
+            jsxFactory: '_React.createElement',
+            jsxFragment: '_React.Fragment',
         });
         return {
             code: result.outputFiles[0].text,
-            err: ''
-        }
+            err: '',
+        };
     } catch (err: any) {
         return {
             code: '',
             err: err.message,
-        }
+        };
     }
-
-    
-
 };
 
 export default bundle;
